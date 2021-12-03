6a7a0d1a3c64c2822dbd08ab14879dfe4a2908b7
Tamas Szabo <szabotamas088@gmail.com>
2021-12-03 13:26:23 +0100

    Connecting to database

---
+++ includes/db.php
@@ -0,0 +1,17 @@
+<?php 
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_NAME = "cms";
+
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($connection) {
+
+    echo "We are connected!";
+}
+
+
+
+?>
